d157de0547d909cd9c47aa49c46e328c736437fd
matttMP <66797927+matttMP@users.noreply.github.com>
2020-12-26 19:42:07 -0600

    Update netbypass.lua

--- netbypass.lua
+++ netbypass.lua
@@ -1,3 +1,7 @@
+warn[[Full Net Bypass // Loaded
+
+            NOTE: Exploit needs setsimulationradius funtcion to work.
+]]
 game["Run Service"].RenderStepped:connect(function()
     settings().Physics.AllowSleep = false
     setsimulationradius(math.huge*math.huge,math.huge*math.huge)
